FydeOS/kernel-tinkerboard

CHROMIUM: DROP: mm/oom_kill: Double-check before killing a child in our place
DROP THIS PATCH ON REBASE.

It is part of a patch series that attempts to solve similar problems
to the OOM reaper upstream.  NOTE that the OOM reaper patches weren't
backported because mm patches in general are fairly intertwined and
picking the OOM reaper without an mm expert and lots of careful
testing could cause new mysterious problems.

Compared to the OOM reaper, this patch series:
+ Is simpler to reason about.
+ Is not very intertwined to the rest of the system.
+ Can be fairly easily reasoned about to see that it should do no
  harm, even if it doesn't fix every problem.
- Can more easily get into the state where all memory reserves are
  gone, since it allows more than one task to be in "MEMDIE".
- Doesn't free memory as quickly.  The reaper kills anonymous /
  swapped memory even before the doomed task exits.
- Contains the magic "100 ms" heuristic, which is non-ideal.

---

Of the child of an OOM victim has a different "->mm" element than the
victim, we may choose to kill the child in place of the victim.  This
is sane because if we killed the victim the child would die anyway, so
why not see if we can save the parent?

...but, before we kill the child let's double-check that it's OK by
calling oom_scan_process_thread().

I'll admit that as the code stands right now this is a totally useless
thing to do.  The only time oom_scan_process_thread() will tell us not
to kill the child is if:

1. The child is already marked with MEMDIE.  However, that's not
   possible because the code (as it stands today) won't let more than
   one process have MEMDIE.
2. The child has no "mm".  ...but that shouldn't happen because the
   parent and child shouldn't share an "mm" if the child's "mm" is
   NULL.

Now that I've explained why this patch is stupid and useless, here's
why we want it anyway: in a future patch we'll make it (sometimes)
possible for more than one process to be marked with MEM

mm/oom_kill.c
@@ -551,9 +551,18 @@ void oom_kill_process(struct oom_control *oc, struct task_struct *p,
 	for_each_thread(p, t) {
 		list_for_each_entry(child, &t->children, sibling) {
 			unsigned int child_points;
+			enum oom_scan_t scan_result;
 
 			if (process_shares_mm(child, p->mm))
 				continue;
+
+			/* Make sure no objections to killing the child */
+			scan_result = oom_scan_process_thread(oc, child,
+							      totalpages);
+			if (scan_result == OOM_SCAN_CONTINUE ||
+			    scan_result == OOM_SCAN_ABORT)
+				continue;
+
 			/*
 			 * oom_badness() returns 0 if the thread is unkillable
 			 */

dmoe1975/android_kernel_moto_shamu-jan28

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

mm/oom_kill.c
@@ -460,9 +460,18 @@ void oom_kill_process(struct task_struct *p, gfp_t gfp_mask, int order,
 	for_each_thread(p, t) {
 		list_for_each_entry(child, &t->children, sibling) {
 			unsigned int child_points;
+			enum oom_scan_t scan_result;
 
 			if (child->mm == p->mm)
 				continue;
+
+			/* Make sure no objections to killing the child */
+			scan_result = oom_scan_process_thread(child, totalpages,
+				nodemask, false);
+			if (scan_result == OOM_SCAN_CONTINUE ||
+			    scan_result == OOM_SCAN_ABORT)
+				continue;
+
 			/*
 			 * oom_badness() returns 0 if the thread is unkillable
 			 */